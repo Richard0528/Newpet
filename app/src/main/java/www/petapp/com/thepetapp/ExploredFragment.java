ba8b2a9e8aa05d22a70847b360eda450265be498
Richard0528 <richardyangzhihao@gmail.com>
2018-06-23 14:18:44 -0700

    add imageSlider

--- app/src/main/java/www/petapp/com/thepetapp/ExploredFragment.java
+++ app/src/main/java/www/petapp/com/thepetapp/ExploredFragment.java
@@ -2,10 +2,16 @@ package www.petapp.com.thepetapp;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import www.petapp.com.thepetapp.model.ImageSliderAdapter;
 
 
 /**
@@ -13,17 +19,35 @@ import android.view.ViewGroup;
  */
 public class ExploredFragment extends Fragment {
 
+    private ViewPager viewPager;
+    ArrayList<Integer> images;
+    ImageSliderAdapter imageSliderAdapter;
 
     public ExploredFragment() {
         // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explored, container, false);
+        View view = inflater.inflate(R.layout.fragment_explored, container, false);
+
+        images = new ArrayList<>();
+        images.add(R.drawable.images_1);
+        images.add(R.drawable.images_2);
+        images.add(R.drawable.images_3);
+        images.add(R.drawable.images_4);
+
+        viewPager = (ViewPager)view.findViewById(R.id.new_pets_pager);
+        TabLayout tabLayout = view.findViewById(R.id.tabDots);
+        imageSliderAdapter = new ImageSliderAdapter(getContext(), images);
+
+        viewPager.setAdapter(imageSliderAdapter);
+        tabLayout.setupWithViewPager(viewPager, true);
+
+        return view;
     }
 
 }
