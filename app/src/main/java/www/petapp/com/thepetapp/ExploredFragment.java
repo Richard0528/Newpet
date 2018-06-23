fb3a108e7de95e84e4285accc8efebbf2cbe1609
Richard0528 <richardyangzhihao@gmail.com>
2018-06-23 15:41:17 -0700

    new recycleview and xml

--- app/src/main/java/www/petapp/com/thepetapp/ExploredFragment.java
+++ app/src/main/java/www/petapp/com/thepetapp/ExploredFragment.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+import www.petapp.com.thepetapp.model.HomeRecycleViewAdapter;
 import www.petapp.com.thepetapp.model.ImageSliderAdapter;
 
 
@@ -20,8 +23,12 @@ import www.petapp.com.thepetapp.model.ImageSliderAdapter;
 public class ExploredFragment extends Fragment {
 
     private ViewPager viewPager;
-    ArrayList<Integer> images;
-    ImageSliderAdapter imageSliderAdapter;
+    private RecyclerView mTopBreederRecyclerView;
+    private ArrayList<Integer> mNewPetsImages;
+    private ArrayList<Integer> mTopBreederImages;
+    private ArrayList<String> mTopBreederTexts;
+    private ImageSliderAdapter imageSliderAdapter;
+    private HomeRecycleViewAdapter homeRecycleViewAdapter;
 
     public ExploredFragment() {
         // Required empty public constructor
@@ -34,18 +41,40 @@ public class ExploredFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_explored, container, false);
 
-        images = new ArrayList<>();
-        images.add(R.drawable.images_1);
-        images.add(R.drawable.images_2);
-        images.add(R.drawable.images_3);
-        images.add(R.drawable.images_4);
+        mNewPetsImages = new ArrayList<>();
+        mNewPetsImages.add(R.drawable.images_1);
+        mNewPetsImages.add(R.drawable.images_2);
+        mNewPetsImages.add(R.drawable.images_3);
+        mNewPetsImages.add(R.drawable.images_4);
 
         viewPager = (ViewPager)view.findViewById(R.id.new_pets_pager);
         TabLayout tabLayout = view.findViewById(R.id.tabDots);
-        imageSliderAdapter = new ImageSliderAdapter(getContext(), images);
+        imageSliderAdapter = new ImageSliderAdapter(getContext(), mNewPetsImages);
 
         viewPager.setAdapter(imageSliderAdapter);
         tabLayout.setupWithViewPager(viewPager, true);
+
+
+        // Top breeder recyclerview
+        mTopBreederImages = new ArrayList<>();
+        mTopBreederImages.add(R.drawable.images_1);
+        mTopBreederImages.add(R.drawable.images_2);
+        mTopBreederImages.add(R.drawable.images_3);
+        mTopBreederImages.add(R.drawable.images_4);
+
+        mTopBreederTexts = new ArrayList<>();
+        mTopBreederTexts.add("111111111111111");
+        mTopBreederTexts.add("222222222222222");
+        mTopBreederTexts.add("333333333333333");
+        mTopBreederTexts.add("444444444444444");
+
+        mTopBreederRecyclerView = view.findViewById(R.id.top_breeder_recycleview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.HORIZONTAL, false);
+        mTopBreederRecyclerView.setLayoutManager(linearLayoutManager);
+
+        homeRecycleViewAdapter = new HomeRecycleViewAdapter(mTopBreederImages, mTopBreederTexts);
+        mTopBreederRecyclerView.setAdapter(homeRecycleViewAdapter);
 
         return view;
     }
--- app/src/main/java/www/petapp/com/thepetapp/model/HomeRecycleViewAdapter.java
+++ app/src/main/java/www/petapp/com/thepetapp/model/HomeRecycleViewAdapter.java
@@ -1,6 +1,57 @@
 package www.petapp.com.thepetapp.model;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomeRecycleViewAdapter extends RecyclerView.Adapter {
+import java.util.List;
+
+import www.petapp.com.thepetapp.R;
+
+public class HomeRecycleViewAdapter extends RecyclerView.Adapter<HomeRecycleViewAdapter.ViewHolder> {
+
+    private List<Integer> images;
+    private List<String> texts;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mImageView;
+        private TextView mTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.home_recycleview_image);
+            mTextView = itemView.findViewById(R.id.home_recycleview_text);
+        }
+    }
+
+    public HomeRecycleViewAdapter(List<Integer> images, List<String> texts) {
+        this.images = images;
+        this.texts = texts;
+    }
+
+    @Override
+    public HomeRecycleViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.top_breeder_recycleview_item, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mImageView.setImageResource(images.get(position));
+        holder.mTextView.setText(texts.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
 }
